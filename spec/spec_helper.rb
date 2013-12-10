2c6b0feadfd7f3cfed3023d7ce3734b8e3b7102e
Nick Johannsen <nick@richarddawkins.net>
2013-12-10 13:48:47 -0800

    attempting to update travis to fix exit issue for travis 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,6 @@
 
 ENV['RACK_ENV'] = 'test'
 
-#suggest to have respect return 1 if test fails
-module Kernel
-  alias :__at_exit :at_exit
-  def at_exit(&block)
-    __at_exit do
-      exit_status = $!.status if $!.is_a?(SystemExit)
-      block.call
-      exit exit_status if exit_status
-    end
-  end
-end
-
-
 SPEC_ROOT = File.join(File.dirname(__FILE__))
 require "#{SPEC_ROOT}/../app/boot"
 require "#{SPEC_ROOT}/faker_extras.rb"
