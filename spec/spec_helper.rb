10634711fbc757c57ba8561ab7deaa3f29f2694f
Nick Johannsen <nick@richarddawkins.net>
2013-12-10 12:05:20 -0800

    Updated to ruby 2.0.0 to see if exit bug is fixed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,15 +23,15 @@ RSpec.configure do |config|
 end
 
 #suggest to have respect return 1 if test fails
-if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
-  module Kernel
-    alias :__at_exit :at_exit
-    def at_exit(&block)
-      __at_exit do
-        exit_status = $!.status if $!.is_a?(SystemExit)
-        block.call
-        exit exit_status if exit_status
-      end
-    end
-  end
-end
+#if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
+#  module Kernel
+#    alias :__at_exit :at_exit
+#    def at_exit(&block)
+#      __at_exit do
+#        exit_status = $!.status if $!.is_a?(SystemExit)
+#        block.call
+#        exit exit_status if exit_status
+#      end
+#    end
+#  end
+#end
