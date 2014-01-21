cb9167654c508a0c09f9e064b2f27b578b93a147
Nick Johannsen <nick@richarddawkins.net>
2014-01-21 14:26:57 -0800

    Update base to be loaded first in boot

--- app/boot.rb
+++ app/boot.rb
@@ -13,5 +13,7 @@ MongoMapper.connection = Mongo::MongoClient.from_uri(ENV['DATABASE_URL'])
 db_name = ENV['DATABASE_URL'][%r{/([^/\?]+)(\?|$)}, 1]
 MongoMapper.database = db_name
 
+require "#{APP_ROOT}/controllers/api/base.rb"
 Dir["#{APP_ROOT}/controllers/api/*.rb"].each {|file| require file }
+require "#{APP_ROOT}/controllers/base.rb"
 Dir["#{APP_ROOT}/controllers/*.rb"].each {|file| require file }
