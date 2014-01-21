d9c37402695b6c45d668b3b55900e8c025857916
Nick Johannsen <nick@richarddawkins.net>
2014-01-21 14:07:45 -0800

    Fixed bug in calendar

--- app/lib/calendar.rb
+++ app/lib/calendar.rb
@@ -1,21 +1,22 @@
 module SC
   class Calendar
-  include Icalendar
-
-  def initialize events
-    @events = events 
-    @cal = Calendar.new
-    events.each do |e|
-      @cal.event do
-        dtstart     e.start_at
-        dtend       e.end_at
-        summary     e.name
-        description e.description
+    include Icalendar
+    
+    def initialize events
+      @events = events 
+      @cal = Calendar.new
+      events.each do |e|
+        @cal.event do
+          dtstart     e.start_at
+          dtend       e.end_at
+          summary     e.name
+          description e.description
+        end
       end
     end
-  end
-
-  def to_ical
-    @cal.to_ical
+    
+    def to_ical
+      @cal.to_ical
+    end
   end
 end
