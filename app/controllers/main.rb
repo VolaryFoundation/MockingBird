b8bc334c516f17b506a2dc03f765da43b176d376
Nick Johannsen <nick@richarddawkins.net>
2014-01-30 12:36:40 -0800

    Added basic user session to system

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -3,6 +3,29 @@ module SC
     get "/" do
       redirect to('/groups')
     end
+    
+    get "/login" do ##new
+      haml :"main/login"
+    end
+    
+    put "/login" do
+      user = User.authenticate(params[:user][:email], params[:user][:password])
+      if user.present?
+        session[:user_id] = user.id
+        flash[:notice] = "You are now loged in"
+        redirect "../groups"
+      else
+        flash[:alert] = "Invalid email or password"
+        haml :"/login"
+      end
+    end 
+    
+    delete "/logout" do
+      session[:user_id] = nil
+      flash[:notice] = "You are loged out"
+      redirect "../groups"
+    end
+
 
     
   end
