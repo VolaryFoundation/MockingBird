b5f8b2dfb83718d37b302941fd06cd703f7b1fc4
Nick Johannsen <nick@richarddawkins.net>
2014-02-05 16:23:41 -0800

    Chnaged env to only case for test and development

--- env.rb
+++ env.rb
@@ -2,7 +2,7 @@ if ENV['RACK_ENV'] == 'test'
   ENV['DATABASE_URL'] = 'mongodb://localhost:27017/volary_api_test'
   ENV['EAGLE_SERVER'] = 'http://volary-eagle-staging.herokuapp.com/'
   ENV['WIDGET_SERVER'] = 'http://volary-pigeon-staging.herokuapp.com/'
-else
+elsif ENV['RACK_ENV'] == 'development'
   ENV['DATABASE_URL'] = 'mongodb://localhost:27017/volary_api_development'
   ENV['EAGLE_SERVER'] = 'http://localhost:3000/'
   ENV['WIDGET_SERVER'] = 'http://volary-pigeon-staging.herokuapp.com/'
