1f582e7ab6f3270312f509ce8dd52e69de589f4b
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-02-26 14:03:32 -0800

    Added 2nd request test for login

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -9,4 +9,14 @@ describe "Auth System" do
     click_button('Login')
     expect(page).to have_content 'Invalid email or password'
   end
+
+  it 'should login in a valid user' do
+    user = create :user
+    visit '/login'
+    fill_in 'user[email]',  :with => user.email
+    fill_in 'user[password]',   :with => user.password
+    click_button('Login')
+    expect(page).to have_content 'You are now loged in'
+    expect(page).to have_content user.email
+  end
 end
