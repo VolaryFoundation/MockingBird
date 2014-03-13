fdeefd756ea37ee8cc11926aa0f955463043dd98
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-03-13 15:22:26 -0700

    Added in URL check in link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,5 +5,53 @@ class Link
   key :name, required: true
   key :type
 
-  
+  validate :custom_validation
+
+  def self.url_connect?(url)
+    new_url = url
+    new_url = url_set_http(new_url) if !new_url.starts_with?('http')
+    begin
+      responce = RestClient.get(new_url)
+    rescue
+      new_url = url_secure_switch(url)
+      begin
+        responce = RestClient.get(new_url)
+      rescue
+      end
+    end
+    if responce.present? && responce.code == 200
+      return ((new_url != url) ?  [200, 'changed', new_url] : [200, true, url])
+    elsif responce.present? && responce.code != 200
+      return [responce.code, 'false', url]
+    else
+      return [400, 'false', url]
+    end
+  end
+
+    
+    private
+
+    def self.url_set_http(url)
+      if !url.starts_with?('http')
+        new_url = "http://#{url}"
+        return new_url
+      else
+        return url
+      end
+    end
+
+
+    def self.url_secure_switch()
+      if !url.starts_with?('http')
+        return nil
+      else
+        if url.starts_with?('http://')
+          new_url = url.gsub('http', 'https')
+        elsif url.starts_with?('https://')
+          new_url = url.gsub('https', 'http')
+        end
+        return new_url
+      end
+    end
+
 end
