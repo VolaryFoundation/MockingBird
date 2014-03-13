086e42ec67888f08fa860f128b5fdf20f67658eb
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-03-12 18:25:16 -0700

    Added email when user make a claim to a gorup

--- app/controllers/groups.rb
+++ app/controllers/groups.rb
@@ -85,6 +85,8 @@ module SC
       user = current_user
       if user.present? && group.present?
         group.claim_group(user)
+        @url = "http://directory.secularconnect.org/groups/#{group.id}"
+        Pony.mail(:to => 'netops@volary.org', :subject => 'Group Ownership Claim', :body => haml(:'email/user_claim', layout: false))
         flash[:notice] = "You are attempting to claim this group. An admin will contact you via email as soon as possible."
         redirect back
       else
