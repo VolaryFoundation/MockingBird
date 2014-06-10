7cb66a75024de03b758b1d34a5415fd28869c380
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-06-10 08:59:22 -0700

    Fixed bug where geo cache was not getting IP

--- app/controllers/groups.rb
+++ app/controllers/groups.rb
@@ -9,14 +9,22 @@ module SC
   
     get "/" do
       results = Geocoder.search(request.ip)
-      state = (results.first.state.present? ? results.first.state : 'Colorado')
+      if results.present?
+        state = (results.first.state.present? ? results.first.state : 'Colorado')
+      else
+        state = 'Colorado'
+      end
       @url = "#{ENV['WIDGET_SERVER']}groups-map.html?filters[subject]=groups&filters[keys][location.state]=#{abbreviate(state)}&size=645x600&viewMode=list"
       haml :"groups/index"
     end
     
     get "/map" do
       results = Geocoder.search(request.ip)
-      state = (results.first.state.present? ? results.first.state : 'Colorado')
+      if results.present?
+        state = (results.first.state.present? ? results.first.state : 'Colorado')
+      else
+        state = 'Colorado'
+      end
       @url = "#{ENV['WIDGET_SERVER']}groups-map.html?filters[subject]=groups&filters[keys][location.state]=#{abbreviate(state)}&size=645x600&viewMode=map"
       haml :'groups/map'
     end
