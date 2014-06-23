4acff2190b24be37d81be373ce88fd281559a49b
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-06-23 12:29:09 -0700

    Added in licence info

--- app/boot.rb
+++ app/boot.rb
@@ -20,3 +20,23 @@ require "#{APP_ROOT}/controllers/api/api_base.rb"
 Dir["#{APP_ROOT}/controllers/api/*.rb"].each {|file| require file }
 Dir["#{APP_ROOT}/controllers/*.rb"].each {|file| require file }
 require "#{ROOT}/config/email.rb"
+
+##=========================================================================##
+## This file is part of MockingBird.                                       ##
+##                                                                         ##
+## MockingBird is Copyright 2014 Volary Foundation and Contributors        ##
+##                                                                         ##
+## MockingBird is free software: you can redistribute it and/or modify it  ##
+## under the terms of the GNU Affero General Public License as published   ##
+## by the Free Software Foundation, either version 3 of the License, or    ##
+## at your option) any later version.                                      ##
+##                                                                         ##
+## MockingBird is distributed in the hope that it will be useful, but      ##
+## WITHOUT ANY WARRANTY; without even the implied warranty of              ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       ##
+## Affero General Public License for more details.                         ##
+##                                                                         ##
+## You should have received a copy of the GNU Affero General Public        ##
+## License along with MockingBird.  If not, see                            ##
+## <http://www.gnu.org/licenses/>.                                         ##
+##=========================================================================##
